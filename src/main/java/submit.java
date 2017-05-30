edf073c5bf1b402afe2de00ee4af2f90736e255d
praveenkayyala <praveenkayyala@gmail.com>
2017-05-30 14:35:35 +0100

    cng
    kjsdkksj

--- src/main/java/submit.java
+++ src/main/java/submit.java
@@ -7,7 +7,7 @@ public class submit extends Basemain  {
     public verifyAC sub(){
 
         driver.findElement(By.xpath("//*[@id='form-validate']/div/button[2]")).click();
-        System.out.println("this page click on submit //////");
+        System.out.println("this page click on submit kkkkkkkkkkkkkkkkkkk");
 return new verifyAC();
 }
 }
